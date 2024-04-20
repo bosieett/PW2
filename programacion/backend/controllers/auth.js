267512ac9f4115ac7231b2b5653de6a33e3636f9
woniewinkeu <cassandraafloress3@gmail.com>
2024-04-20 12:03:03 -0600

    login and register pages

--- programacion/backend/controllers/auth.js
+++ programacion/backend/controllers/auth.js
@@ -56,3 +56,25 @@ export const login = async(req, res) => {
         res.status(500).json({error: err.message});
     }
 }
+
+export const verifyToken = async (req, res, next)=>{
+    try{
+        let token = req.header("Authorization");
+        if (!token){
+            return res.status(403).send("Access Denied");
+
+        }
+
+        if (token.startWidth("Bearer")){
+            token = token.slice (7, token.length).trimLeft();
+
+        }
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+
+    } catch (err){
+        res.status(500).json({error: err.message});
+    }
+};
--- programacion/client/src/scenes/homePage/index.jsx
+++ programacion/client/src/scenes/homePage/index.jsx
@@ -2,9 +2,25 @@ import { Box } from "@mui/material";
 import Navbar from "scenes/navbar";
 
 const HomePage = () =>{
+const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+const {_id, picturePath}= useSelector((state) => state.user);
+
+
+
     return ( 
         <Box>
             <Navbar/>
+            <Box
+            width="100%"
+            padding="2rem 6%"
+            display= {isNonMobileScreens ? "flex": "block"}
+            gap= "0.5rem"
+            justifyContent="space-between"
+            >
+                <Box flexBasis={isNonMobileScreens ? "26%": undefineed}>
+                    <UserWidget userId={_id} picturePath={picturePath}/>
+                </Box>
+            </Box>
         </Box>
     );
 }
