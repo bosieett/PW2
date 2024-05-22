279856f0439d0ccf769b7c6d9553381874d4d7de
Kevin Sánchez <124293070+bosieett@users.noreply.github.com>
2024-05-22 15:59:47 -0600

    Update PetProfileWidget.jsx

--- programacion/client/src/scenes/widgets/PetProfileWidget.jsx
+++ programacion/client/src/scenes/widgets/PetProfileWidget.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Box, Typography, Paper, Avatar } from '@mui/material';
+import { Box, Typography, Paper, Avatar, Button, Divider } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { resetPet, setPet } from 'state';
 
@@ -16,6 +16,7 @@ const AnimalProfile = ({ petId }) => {
 
     const data = await response.json();
     dispatch(setPet({ pet: data}));
+    console.log(pet);
 }
 useEffect(() => {
   getPet();
@@ -26,16 +27,38 @@ useEffect(() => {
   }
 
   return (
-    <Box>
-    <Paper elevation={3}>
-        <Avatar src={pet.imageUrl} alt={pet.name} />
-        <Typography variant="h5">{pet.name}</Typography>
-        <Typography variant="body1">Edad: {pet.age}</Typography>
-        <Typography variant="body1">Sexo: {pet.gender}</Typography>
-        <Typography variant="body1">Tamaño: {pet.size}</Typography>
-        <Typography variant="body1">{pet.description}</Typography>
-    </Paper>
-</Box>
+    <Box sx={{ p: 2 }}>
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mb: 2 }}>
+        <Avatar
+          alt={pet.petName}
+          src={`http://localhost:3001/assets/${pet.picturePath}`}
+          sx={{ width: 200, height: 200 }}
+          variant="rounded"
+        />
+      </Box>
+
+      <Paper elevation={3} sx={{ p: 2, textAlign: 'center' }}>
+        <Typography variant="h4">{pet.petName}</Typography>
+        <Typography variant="body1">Edad: {pet.petAge}</Typography>
+        <Typography variant="body1">Sexo: {pet.petGender}</Typography>
+        <Typography variant="body1">Tamaño: {pet.petSize}</Typography>
+        <Typography variant="body1" sx={{ mt: 1 }}>{pet.petDescription}</Typography>
+        <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+          ¡Quiero darle un hogar!
+        </Button>
+      </Paper>
+{/*
+      <Box sx={{ mt: 4 }}>
+        <Typography variant="h6">PUBLICACIONES</Typography>
+        <Divider />
+        {posts.map((post, index) => (
+          <Box key={index} sx={{ mt: 2 }}>
+            <Typography variant="body2">{post.content}</Typography>
+            <Divider sx={{ mt: 1 }} />
+          </Box>
+        ))}
+      </Box>*/}
+    </Box>
   );
 };
 
